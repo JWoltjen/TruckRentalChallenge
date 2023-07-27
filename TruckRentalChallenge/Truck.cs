1ba094b52f00968309b5061cd0867aff7e4f5185
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-07-27 10:23:08 -0500

    Create initial class structure for Truck

---
+++ TruckRentalChallenge/Truck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckRentalChallenge
+{
+    public class Truck
+    {
+        public double WeekdayRate { get; set; }
+        public double WeekendRate { get; set; }
+        public double FirstTwentyMinutes { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+
+        public Truck(double weekdayRate, double weekendRate, double firstTwentyMinutes)
+        {
+            this.WeekdayRate = weekdayRate;
+            this.WeekendRate = weekendRate;
+            this.FirstTwentyMinutes = firstTwentyMinutes;
+        }
+        public double CalculatePrice()
+        {
+            return 15; 
+        }
+    }
+}
